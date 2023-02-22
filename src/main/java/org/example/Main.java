efa5b4526fdcb7ff0a7e690debc321fc9b6026f9
Raghavendra <rahulhhh15@gmail.com>
2023-02-21 23:02:48 -0800

    Added csv file and made use of Jena library

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        CsvToRdfConverter converter = new CsvToRdfConverter();
+        try {
+            converter.convert("src/main/java/org/example/City_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv", "output.rdf");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
